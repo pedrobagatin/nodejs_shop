5793d465c541b9bb1e48e1088cf4e75ad024504f
Pedro B <pedrobagatin@yahoo.com.br>
2021-09-10 19:00:32 -0300

    Added handlebars style and layout

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs= require('express-handlebars')
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine(
+    "hbs",
+    expressHbs({
+      extname: "hbs",
+      defaultLayout: 'main-layout',
+      layoutsDir: "views/layouts/"
+    })
+  );
+
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
